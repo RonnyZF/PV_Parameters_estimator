33791b8d1f3e5c493c3c8c7e766b0dd019350686
Ronny <ronnyzarate@gmail.com>
2018-12-12 16:11:47 -0600

    Rango de log acotado de 0 a 10

--- Zedboard/tb_log_calc.cpp
+++ Zedboard/tb_log_calc.cpp
@@ -9,16 +9,17 @@ int main(){
 	hls::stream<ADC_data> in;
 	hls::stream<log_data> out;
 	gen_samples(in);
-	for (int i=1;i<25;i++){
+	float a = 0.02;
+	for (int i=1;i<500;i++){
 		fixed_log(in,out);
-		float comp = log(i);
+		float comp = log(a);
 		log_data resultado = out.read();
-
 		//calculos de error
 		float log_float = resultado.log;
 		float error = ((log_float-comp)/comp);
-
-		std::cout << "log("<<i<<"): \t Resultado obtenido: "<< resultado.log << "\t esperado: " << comp << "\t %error: "<<error<<std::endl;
+		//std::cout << "log("<<resultado.adc_v<<"): \t Resultado obtenido: "<< resultado.log << "\t esperado: " << comp << "\t %error: "<<error<<std::endl;
+		std::cout << a<<","<<resultado.log << "," << comp<<std::endl;
+		a=a+0.02;
 	}
 	return 0;
 }
@@ -27,9 +28,9 @@ int main(){
 	 ADC_data samples;
 	samples.adc_i=0;
 	samples.adc_v=0;
-	fixed_32 a=1;
-	fixed_32 b=1;
-	for (int i=1;i<25;i++){
+	fixed_32 a=0.02;
+	fixed_32 b=0.02;
+	for (int i=1;i<500;i++){
 		samples.adc_v += a;
 		samples.adc_i += b;
 		in.write(samples);
