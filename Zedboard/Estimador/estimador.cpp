bce996050b155d726bb887841f50cf422c4594f1
Ronny <ronnyzarate@gmail.com>
2019-01-17 16:14:20 -0600

    se añade proceso de calculo de logaritmo al estimador

--- Zedboard/Estimador/estimador.cpp
+++ Zedboard/Estimador/estimador.cpp
@@ -5,9 +5,36 @@
 
 int fixed_estimador(hls::stream<adc_data<fixed_32 > > &in, hls::stream<param_t<fixed_32 > > &out){
 # pragma HLS DATAFLOW
-	template_estimador<fixed_32 > (in,out);
+	hls::stream<adc_data<fixed_log_pres> > in_log;
+	adc_data<fixed_32> sample_in_log=in.read();
+	adc_data<fixed_log_pres> aux = {0,0};
+	aux.adc_v=sample_in_log.adc_v;
+	aux.adc_i=sample_in_log.adc_i;
+	in_log.write(aux);
+	std::cout<<"aux_v="<<aux.adc_v<< " in_log_v="<<sample_in_log.adc_v<<std::endl;
+	std::cout<<"aux_i="<<aux.adc_i<< " in_log_i="<<sample_in_log.adc_i<<std::endl;
+
+
+	hls::stream<log_data<fixed_log_pres> > out_log;
+
+	template_fixed_log<Scaling,fixed_log_pres>(in_log,out_log);
+
+	hls::stream<adc_data<fixed_32> > in_est;
+
+	log_data<fixed_log_pres> sample_in_est=out_log.read();
+	std::cout<<"sample_in_est_v="<<sample_in_est.adc_v<<std::endl;
+	std::cout<<"sample_in_est_i="<<sample_in_est.log<<std::endl;
+	adc_data<fixed_32> aux2;
+	aux2.adc_v=sample_in_est.adc_v;
+	aux2.adc_i=sample_in_est.log;
+	in_est.write(aux2);
+
+	template_estimador<fixed_32 > (in_est,out);
 	return 0;
 }
+
+
+
 
 
 
--- Zedboard/Estimador/estimador.hpp
+++ Zedboard/Estimador/estimador.hpp
@@ -6,6 +6,7 @@
 #include <stdint.h>
 
 typedef ap_fixed<32,8,AP_RND,AP_SAT> fixed_32;
+typedef ap_fixed<32,14,AP_RND,AP_SAT> fixed_log_pres;
 
 // --------------------------------------------------------
 template<typename T>
@@ -20,9 +21,21 @@ struct param_t{
 	T _2;
 };
 
+template<typename T>
+struct log_data{
+	T adc_v;
+	T log;
+};
+
+struct Scaling{
+	fixed_32 x;
+	fixed_32 y;
+ };
+
 /* ****************************** C++ TEMPLATES ***************************************/
 
 int fixed_estimador(hls::stream<adc_data<fixed_32 > > &in, hls::stream<param_t<fixed_32 > > &out);
+int fixed_log(hls::stream<adc_data<fixed_32 > > &in, hls::stream<log_data<fixed_32 > > &out);
 
 template<typename T>
  int template_estimador(hls::stream<adc_data<T > > &in, hls::stream<param_t<T > > &out){
@@ -69,11 +82,75 @@ int template_samples_generator(hls::stream< adc_data<T > > &in, int n){
 	static int i = 0;
 	volt = V_CTE+K*V_CTE*sin(2*MM_PI*PVG_F*i/F_SAMPLING);
 	current = ALPHA*volt +b;
-	samples.adc_i=current;
+
+
+	samples.adc_i=(current<1) ?current*-1: current;
 	samples.adc_v=volt;
 	i=n;
-	//std::cout << "volt: " << samples.adc_v << "\t current: " << samples.adc_i << std::endl;
+
 	in.write(samples);
 	return 0;
 }
 
+// --------------------------------------------------------
+template<typename T>
+T template_approxLn(T log2_result){
+	const T log2_e=1.442695041;
+	return log2_result/log2_e;
+}
+
+// --------------------------------------------------------
+template<typename P, typename T>
+P template_escalamiento(T x){
+	T y = 0;
+	bool not_finished_scaling = true;
+	while (not_finished_scaling){
+#pragma HLS loop_tripcount min=1 max=3 avg=2
+		if (x<1){
+			x=2*x;
+			y=y-1;
+
+		}
+		else if(x>=2){
+			x=x/2;
+			y=y+1;
+		}
+		else{
+			not_finished_scaling = false;
+		}
+	}
+	P result = {x, y};
+	return result;
+
+}
+
+// --------------------------------------------------------
+template<typename T>
+T template_approxLog2(T x, T y){
+	T b=0.5;
+		for (int i = 0; i < 15; ++i){
+			x=x*x;
+			if (x>=2){
+				x=x/2;
+				y=y+b;
+			}
+			b=b/2;
+		}
+	return y;
+}
+
+// --------------------------------------------------------
+template<typename P, typename T>
+int template_fixed_log(hls::stream<adc_data<T> > &in, hls::stream<log_data<T> > &out){
+#pragma HLS DATAFLOW
+	adc_data<T> sample_in;
+	log_data<T> sample_out;
+	sample_in=in.read();
+	std::cout<<"adc_i: "<<sample_in.adc_i<<std::endl;
+	P aux = template_escalamiento<P,T>(sample_in.adc_i);
+	T aux2 = template_approxLog2<T>(aux.x,aux.y);
+	sample_out.log = template_approxLn<T>(aux2);
+	sample_out.adc_v = sample_in.adc_v;
+	out.write(sample_out);
+	return 0;
+}
